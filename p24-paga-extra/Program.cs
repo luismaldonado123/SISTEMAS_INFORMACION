27f32650961e8e141fc93850ac43036368dc335f
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-21 11:35:04 -0600

    Add files via upload

---
+++ p24-paga-extra/Program.cs
@@ -0,0 +1,14 @@
+﻿// Calcular la paga de un trabajador
+int horas, extra;
+float paga, total;
+Console.Clear();
+Console.WriteLine("Calculando la paga de un trabajador \n");
+Console.Write("Dame las horas ? "); horas=int.Parse(Console.ReadLine());
+Console.Write("Dame la paga ? "); paga=float.Parse(Console.ReadLine());
+if( horas > 40 ) {
+extra = horas - 40;
+total = (40*paga) + (extra * paga * 2);
+Console.WriteLine($"\nHoras extra: {extra}");
+}
+else total = horas * paga;
+Console.WriteLine($"\nPago total: {total:f2}");
