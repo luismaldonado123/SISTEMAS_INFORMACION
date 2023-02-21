08b3ff34ee4c249042997b58afee6132428c812c
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-21 11:02:55 -0600

    Add files via upload

---
+++ p22-verifica-suma/Program.cs
@@ -0,0 +1,12 @@
+﻿// Verifica si la suma de dos numeros es igual a un tercero
+int n1, n2, n3;
+Console.Clear();
+Console.WriteLine("Verificar si la suma de dos numeros es igual a un tercero\n");
+Console.Write("Dame el primer numero ? "); n1=int.Parse(Console.ReadLine());
+Console.Write("Dame el seguno numero ? "); n2=int.Parse(Console.ReadLine());
+Console.Write("Dame el tercer numero ? "); n3=int.Parse(Console.ReadLine());
+if( n1+n2 == n3 )
+Console.WriteLine("Son iguales");
+else
+Console.WriteLine("Son distintos");
+Console.WriteLine("\nGracias por utilizar este programa");
