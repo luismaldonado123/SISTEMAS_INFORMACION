1b813a7d34ea668a6c0eda77145057c6710cf7d6
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-21 10:57:00 -0600

    Add files via upload

---
+++ p21-verificar-numero/Program.cs
@@ -0,0 +1,10 @@
+﻿// verifica si un numero es positivo, negativo o cero
+int n;
+Console.WriteLine("verifica si un numero es positivo, negativo o cero");
+Console.WriteLine("dame un numero?:");
+n = int.Parse(Console.ReadLine());
+if(n>0) Console.WriteLine("el numero es positivo..");
+if(n<0) Console.WriteLine("el numero es negativo..");
+if(n==0) Console.WriteLine("el numero es cero..");
+
+Console.WriteLine("Gracias por utilizar este programa");
