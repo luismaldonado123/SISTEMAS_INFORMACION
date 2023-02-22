5ebd66a6f90398c0c67ca4e244c0ea96378a9a26
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-21 21:41:47 -0600

    Add files via upload

---
+++ p17-convertir-temperatura/Program.cs
@@ -0,0 +1,7 @@
+﻿//programa que convierte de grados fahrenheit a celcius
+double celcius, fahrenheit;
+Console.WriteLine("programa que convierte de gradis fahrenheit a celcius...");
+Console.WriteLine("dame los grados fahrenheit:");
+fahrenheit = double.Parse(Console.ReadLine());
+celcius = (fahrenheit -32) * 5/9;
+Console.WriteLine($"tus grados celcius son:{celcius}");
