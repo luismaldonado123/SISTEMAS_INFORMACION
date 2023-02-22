355252cbaff4794ca5056030a8ed0746074bcb0c
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-21 21:43:19 -0600

    Add files via upload

---
+++ p19-calculo-tiempo/Program.cs
@@ -0,0 +1,9 @@
+﻿//programa que calcula el tiempo.
+double dias=0, horas=0, minutos=0, segundos=0;
+Console.WriteLine("programa que calcula el tiempo..");
+Console.Write("Ingresa las horas");
+horas = double.Parse(Console.ReadLine());
+dias = horas / 24;
+minutos = horas * 60;
+segundos = horas * 60 * 60;
+Console.WriteLine($"el tiempo calculado es: {dias} dias, {horas} horas, minutos {minutos}, segundos {segundos}");
