849fd94a2facc4baf00126baa61ca4ed92fbbacd
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-21 21:43:49 -0600

    Add files via upload

---
+++ p20-conversion-peso-dolar/Program.cs
@@ -0,0 +1,10 @@
+﻿// programa que convierte de pesos a dolares.
+double dolar, peso, equivalente;
+Console.WriteLine("programa que convierte de pesos a dolares");
+Console.WriteLine("ingrese la cantidad en pesos mexicanos");
+peso = Double.Parse(Console.ReadLine());
+Console.WriteLine("dame el precio del dolar:");
+dolar = Double.Parse(Console.ReadLine());
+equivalente = peso/dolar;
+Console.WriteLine($"el equivalente de pesos en dolares es:{equivalente}");
+
