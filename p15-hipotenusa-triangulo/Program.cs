0ea33bb40b55288b724b8a6012aa1866f62f2e0c
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-21 21:40:30 -0600

    Add files via upload

---
+++ p15-hipotenusa-triangulo/Program.cs
@@ -0,0 +1,10 @@
+﻿// Calcular la hipotenusa de un triangulo rectangulo dadas las longitudes de sus lados.
+//variables a utilizar.
+double longitudLado1, longitudLado2, hipotenusa;
+Console.WriteLine("programa que calcule la hipotenusa..");
+Console.WriteLine("escriba el valor de LongitudLado1:..");
+longitudLado1 = double.Parse(Console.ReadLine());
+Console.WriteLine("escriba el valor de LongitudLado2:..");
+longitudLado2 = double.Parse(Console.ReadLine());
+hipotenusa = Math.Sqrt(longitudLado1*longitudLado1 + longitudLado2*longitudLado2);
+Console.WriteLine("el valor de la hipotenusa es :"+hipotenusa);
