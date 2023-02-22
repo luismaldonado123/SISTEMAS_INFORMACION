1f8fc083b96bb45ba6a6ce7e836ca55a43cdf64c
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-21 21:42:22 -0600

    Add files via upload

---
+++ p18-volumen-cilindro/Program.cs
@@ -0,0 +1,9 @@
+﻿// programa que calcula el volumen de un cilindro
+double radio, volumen, altura;
+Console.WriteLine("programa que calcula el volumen de un cilindro..");
+Console.WriteLine("ingresa el radio:");
+radio = double.Parse(Console.ReadLine());
+Console.WriteLine("dame la altura:");
+altura = double.Parse(Console.ReadLine());
+volumen = radio*radio*altura*Math.PI;
+Console.WriteLine($"el volumen es: {volumen}");
