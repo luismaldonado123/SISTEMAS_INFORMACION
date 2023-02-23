46e07732357c58e02efbd177941a996ee357109a
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-23 11:09:35 -0600

    Add files via upload

---
+++ p26-tipo-angulo/Program.cs
@@ -0,0 +1,24 @@
+﻿// muestra el tipo de angulo segun los grados
+Console.Clear();
+Console.WriteLine("Muestra el tipo de angulo segun los grados\n");
+Console.Write("Dame un angulo ? ");
+int angulo = int.Parse(Console.ReadLine());
+
+if(angulo < 0 || angulo>360)
+    Console.WriteLine("Angulo invalido..");
+else{
+    if(angulo==0)
+    Console.WriteLine("\nEl angulo es nulo");
+    if(angulo >0 && angulo<90) 
+    Console.WriteLine("\nEs agudo");
+    if(angulo==90) 
+    Console.WriteLine("\nEs recto");
+    if(angulo>90 && angulo<180) 
+    Console.WriteLine("\nEs obtuso");
+    if(angulo==180) 
+    Console.WriteLine("\nEs llano");
+    if(angulo>180 && angulo<360) 
+    Console.WriteLine("\nEs concavo");
+    if(angulo==360) 
+    Console.WriteLine("\nEs completo");
+}
