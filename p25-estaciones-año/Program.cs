436dc65511e0bc6429c9fbd9b93feca8c01350d4
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-02-23 11:06:35 -0600

    Add files via upload

---
+++ p25-estaciones-año/Program.cs
@@ -0,0 +1,11 @@
+﻿// Muestra la estación del año que corresponde al número introducido
+Console.Clear();
+Console.WriteLine("Muestra la estacion del año en base al número:\n");
+Console.Write("Dame un numero (1..4) ? ");
+int n = int.Parse(Console.ReadLine());
+if(n>=1 && n<=4){
+    if(n==1) Console.WriteLine("Primavera\n");
+    if(n==2) Console.WriteLine("Verano\n");
+    if(n==3) Console.WriteLine("Otoño\n");
+    if(n==4) Console.WriteLine("Invierno\n");
+}else Console.WriteLine("\nen que planeta vives?");
