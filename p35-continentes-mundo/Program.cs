bd65f68e9469fad07acc51f2fb974cac84a318aa
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-04 12:13:07 -0600

    Add files via upload

---
+++ p35-continentes-mundo/Program.cs
@@ -0,0 +1,20 @@
+﻿// dado un numero entero entre 1 y 6 que corresponde a un continente del mundo, escribir su nombre
+int validar=0;
+Console.WriteLine("dado un numero entero entre 1 y 6 que corresponde a un continente del mundo, escribir su nombre, ejemplo 1 :Asia");
+Console.WriteLine("Dame un numero del 1 al 6..");
+validar = int.Parse(Console.ReadLine());
+if(validar > 6){
+    Console.WriteLine("Fuera de rango, intente de nuevo");
+}else{
+    switch(validar){
+    case 1 : Console.WriteLine("Asia"); break;
+    case 2 : Console.WriteLine("Africa"); break;
+    case 3 : Console.WriteLine("America del norte"); break;
+    case 4 : Console.WriteLine("America del sur"); break;
+    case 5 : Console.WriteLine("Antartida"); break;
+    case 6 : Console.WriteLine("Europa"); break;
+    default:break;
+    }
+    Console.WriteLine("\n proceso terminado...");
+}
+
