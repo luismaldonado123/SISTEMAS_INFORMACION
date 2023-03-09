9fff571305a256b189a5f378e74d9ead9a5bc43d
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-09 11:29:31 -0600

    Add files via upload

---
+++ p41-numeros-100-1/Program.cs
@@ -0,0 +1,8 @@
+﻿// programa del 1 al 100
+int c=1;
+while(c <=100){
+    Console.Write(c+" ");
+    c++;
+}
+Console.WriteLine("proceso terminado");
+
---
+++ p42-numeros-100-1/Program.cs
@@ -0,0 +1,11 @@
+﻿// numeros del 100 al 1, hacia atras.
+Console.Clear();
+int c,n,p;
+Console.Write("hasta donde ?"); n = int.Parse(Console.ReadLine());
+Console.Write("paso ?"); p = int.Parse(Console.ReadLine());
+c = n;
+while(c >= 1){
+    Console.Write(c+" ");
+    c-=p;
+}
+Console.WriteLine("\nproceso terminado\n");
