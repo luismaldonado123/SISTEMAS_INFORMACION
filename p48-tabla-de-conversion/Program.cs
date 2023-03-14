d2759aed5ac96ed3851d4a16f29b536d34b814f0
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-14 11:24:06 -0600

    Add files via upload

---
+++ p48-tabla-de-conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿// Imprime tabla de conversión de peso a dolar
+int ini, fin, c;
+float tc = 19.99f;
+char resp;
+do {
+    Console.Clear();
+    Console.WriteLine("Imprime una tabla de conversion de peso a dolar\n");
+    do {
+    Console.Write("Inicio : "); ini = int.Parse(Console.ReadLine());
+    Console.Write("Fin : "); fin = int.Parse(Console.ReadLine());
+    } while(fin<ini);
+        c=ini;
+        Console.WriteLine(new string('-',20));
+        Console.WriteLine("Peso\tDolar");
+        Console.WriteLine(new string('-',20));
+while( c<=fin ) {
+    Console.WriteLine($"{c}\t{c/tc:f4}");
+    c++;
+}
+Console.WriteLine(new string('-',20));
+Console.Write("\nDeseas continuar (S/N) ? ");resp = char.ToUpper(Console.ReadLine()[0]);
+} while( resp != 'N');
+Console.WriteLine("\nGracias por utilizar este programa !");
---
+++ p49-conjetura-de-collatzn/Program.cs
@@ -0,0 +1,21 @@
+﻿// Imprime la conjetura de collatz para un número entero positivo
+int n;
+char resp;
+do {
+    Console.Clear();
+    Console.WriteLine("Imprime la conjetura de collatz para un número entero positivo\n");
+    do {
+    Console.Write("Dame un numero positivo ? ");
+    n = int.Parse(Console.ReadLine());
+    } while( n < 0);
+    do {
+    Console.Write($"{n} ");
+    if(n%2==0)
+    n = n / 2;
+    else
+    n = n * 3 + 1;
+} while(n!=1);
+    Console.WriteLine(n);
+    Console.Write("\nDeseas continuar (S/N) ? ");resp = char.ToUpper(Console.ReadLine()[0]);
+} while (resp != 'N');
+    Console.WriteLine("\nGracias por utilizar este programa !");
