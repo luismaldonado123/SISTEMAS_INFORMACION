aaf793d874db5ae63d2788f1fde2bb85dd5dedec
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-16 11:19:57 -0600

    Add files via upload

---
+++ p57-suma-pares-impares/Program.cs
@@ -0,0 +1,39 @@
+﻿// Suma de pares e impare
+int op, n, s, i;
+do {
+    Console.Clear();
+    Console.WriteLine("Impares ascendente .... [ 1 ]");
+    Console.WriteLine("Pares descentente...... [ 2 ]");
+    Console.WriteLine("Salir ....... [ 3 ]");
+    Console.Write("Elije ? "); op=int.Parse(Console.ReadLine());
+    switch(op) {
+    case 1:{
+    s=0;
+    Console.WriteLine("\nImpares ascendenbte y su suma:");
+    Console.Write("Hasta donde ? "); n=int.Parse(Console.ReadLine());
+        for(i=1; i<=n; i+=2) {
+        Console.Write($"{i} ");
+        s+=i;
+    }
+    Console.WriteLine($"\nLa suma de impares es {s}");
+    }break;
+    case 2:{
+    s=0;
+    Console.WriteLine("\nPares descendente y su suma:");
+    Console.WriteLine("Hasta donde ? ");n=int.Parse(Console.ReadLine());
+    n = ( n%2==0 ? n : --n);
+        for(i=n; i>=1; i-=2) {
+        Console.Write($"{i} ");
+        s+=i;
+    }
+    Console.WriteLine($"\nLa suma de pares es {s}");
+    }break;
+    case 3:
+        Console.WriteLine("\nTe vas por que quieres nadie te corre");break;
+    default:
+    Console.WriteLine("\nOpcion Invalida"); 
+    break;
+    Console.WriteLine("\nPresiona cualquier tecla para continuar");Console.ReadLine();
+    }
+} while( op != 3);
+Console.WriteLine("\nProceso terminado ...");
---
+++ p58-suma-promedio/Program.cs
@@ -0,0 +1,21 @@
+﻿// Suma y promedio de n calificaciones
+int n;
+float cal, suma, prom;
+char resp;
+do {
+    Console.Clear();
+    suma=prom=0;
+    Console.WriteLine("Suma y promedio de n calificaciones: \n");
+    Console.Write("Cuantas calificaciones ? ");
+    n=int.Parse(Console.ReadLine());
+    for(int i=1; i<=n; i++) {
+        Console.Write($"Calificacion {i} ? ");
+        cal=float.Parse(Console.ReadLine());
+        suma+=cal;
+    }
+    prom=suma/n;
+    Console.WriteLine($"\nSuma {suma} y promedio {prom}");
+    Console.Write("\nDeseas continuar (S/N) ? ");
+    resp=char.ToUpper(Console.ReadLine()[0]);
+} while( resp != 'N');
+    Console.WriteLine("\nProceso Terminado ...");
