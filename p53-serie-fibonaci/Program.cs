1c7c84b2f530d5abb0659e87a639bfa45c53fd07
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-17 13:35:49 -0600

    Add files via upload

---
+++ p53-serie-fibonaci/Program.cs
@@ -0,0 +1,23 @@
+﻿int numero=0, carry = 0, x = 0, fiboant = 0 , z = 0;
+char resp;
+do {
+    Console.Clear();
+    Console.WriteLine("Imprime los primeros n numeros de la serie de fibonacci\n");
+    do {
+        Console.Write("Ingresa un numero? ");
+        numero = int.Parse(Console.ReadLine());
+    } while( numero <= 0);
+    do {
+        z = fiboant;
+        fiboant = x;
+        x = fiboant + z;
+        Console.Write($"{fiboant,5}\t ");
+        if(x == 0)
+         x++;
+         carry++;
+    } while(carry < numero);
+    carry = x = fiboant = z = 0;
+    Console.Write("\nDeseas continuar (S/N) ? ");
+    resp = char.ToUpper(Console.ReadLine()[0]);
+} while (resp != 'N');
+Console.WriteLine("\nGracias por utilizar este programa !");
