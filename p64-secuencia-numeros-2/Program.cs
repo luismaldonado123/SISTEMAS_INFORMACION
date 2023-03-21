3a272eade4a0a54a1017f7da4ca774f79b2b801f
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-21 15:10:37 -0600

    Add files via upload

---
+++ p64-secuencia-numeros-2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Se desea imprimir la secuencia de números mostrados el número de renglones que el usuario desee
+int numero,i,j;
+char resp;
+do{
+Console.WriteLine("Cuantos renglones ?");
+numero = int.Parse(Console.ReadLine());
+for(i=1; i <=numero; i++){
+    for(j=1; j<=i; j++){
+        Console.Write(j+" ");
+    }
+    Console.WriteLine();
+}
+Console.Write("\n deseas continuar (S/N)?");
+resp = char.ToUpper(Console.ReadLine()[0]);
+}while(resp!='N');
+Console.WriteLine("Proceso terminado");
