f3d09adadd13a1055b047c0ba0609907a189e026
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-23 11:10:27 -0600

    Add files via upload

---
+++ p61-factorial-numeros/Program.cs
@@ -0,0 +1,20 @@
+﻿// Factorial de n números
+int i,j,n,f;
+char resp;
+do {
+    Console.Clear();
+    Console.Write("Cuantos números ? "); n = int.Parse(Console.ReadLine());
+    for(i=1; i<=n; i++) {
+       Console.Write($"{i}!=");
+       f=1;
+       for(j=1; j<=i; j++) {
+           Console.Write($"{j}");
+           if(j!=i)//para no imprimir el ultimo asterisco
+              Console.Write("*");
+        f *= j;
+    }
+Console.Write($" {f} \n");
+}
+Console.Write("\nDeseas continuar (S/N) ?");resp=char.ToUpper(Console.ReadLine()[0]);
+} while( resp!= 'N');
+Console.WriteLine("\nProceso terminado ..");
---
+++ p62-suma-terminos/Program.cs
@@ -0,0 +1,20 @@
+﻿// Suma de n términos de factorial
+int i,j,n;
+float f, r;
+char resp;
+do {
+    Console.Clear();
+    r=0;
+    Console.Write("Cuantos números ? "); n = int.Parse(Console.ReadLine());
+    for(i=1; i<=n; i++) {
+       f=1;
+        for(j=1; j<=i; j++) {
+        f *= j;
+    }   
+    Console.Write($" 1 / {i}! +");
+    r += (1 / f);
+}
+Console.WriteLine($"= {r}");
+Console.Write("\nDeseas continuar (S/N) ?");resp=char.ToUpper(Console.ReadLine()[0]);
+} while( resp!= 'N');
+    Console.WriteLine("\nProceso terminado ..");
