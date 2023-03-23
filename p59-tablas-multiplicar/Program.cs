c9cc169fa8df85f4aeddffb295badfa7165d51b5
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-23 11:09:40 -0600

    Add files via upload

---
+++ p59-tablas-multiplicar/Program.cs
@@ -0,0 +1,16 @@
+﻿// Tablas de multiplicar con de 1 a n hasta la del n
+int i,j,n,m;
+char resp;
+do {
+    Console.Clear();
+    Console.Write("Hasta que tabla ? "); n = int.Parse(Console.ReadLine());
+    Console.Write("Hasta donde ? "); m = int.Parse(Console.ReadLine());
+    for(i=1; i<=n; i++) {
+        Console.WriteLine($"\nTabla del {i}");
+        for(j=1; j<=m; j++) {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+        }
+    }
+Console.WriteLine("\nDeseas continuar (S/N) ?");resp=char.ToUpper(Console.ReadLine()[0]);
+} while( resp!= 'N');
+Console.WriteLine("\nProceso terminado ..");
---
+++ p60-piramider/Program.cs
@@ -0,0 +1,16 @@
+﻿// Imprime una pirámide del caracter
+int i,j,n;
+char car, resp;
+do {
+    Console.Clear();
+    Console.Write("Cuantos renglones ? "); n = int.Parse(Console.ReadLine());
+    Console.Write("De que caracter ? "); car = Console.ReadLine()[0];
+    for(i=1; i<=n; i++) {
+       for(j=1; j<=i; j++) {
+           Console.Write($"{car}");
+        }
+    Console.WriteLine();
+    }
+    Console.Write("\nDeseas continuar (S/N) ?");resp=char.ToUpper(Console.ReadLine()[0]);
+} while( resp!= 'N');
+    Console.WriteLine("\nProceso terminado ..");
