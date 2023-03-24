67acacd50ec61f3dc6b486e8599e07e95d2797d9
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-24 11:13:53 -0600

    Add files via upload

---
+++ p63-secuencia-numeros-1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Se desea imprimir la secuencia de numeros mostrados el numero de renglones que el usuario desee.
+int numero,i,j;
+char resp;
+do{
+Console.WriteLine("Cuantos renglones ?");
+numero = int.Parse(Console.ReadLine());
+for(i=1; i <=numero; i++){
+    for(j=1; j<=i; j++){
+        Console.Write(i+" ");
+    }
+    Console.WriteLine();
+}
+Console.Write("\n deseas continuar (S/N)?");
+resp = char.ToUpper(Console.ReadLine()[0]);
+}while(resp!='N');
+Console.WriteLine("Proceso terminado");
