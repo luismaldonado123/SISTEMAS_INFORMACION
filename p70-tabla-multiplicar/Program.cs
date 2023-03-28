68792a084cced6bc43ccfd3b6f94455346cc201f
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-03-28 11:29:42 -0600

    Add files via upload

---
+++ p70-tabla-multiplicar/Program.cs
@@ -0,0 +1,11 @@
+﻿// Tabla de multiplicar
+void Tabla(int t, int n) {
+    Console.WriteLine($"\nTabla del {t}");
+    for(int i=1; i <= n; i++)
+        Console.WriteLine($"{t} x {i} = {t*i}");
+}
+Console.Write("Que tabla deseas ? ");
+int t = int.Parse(Console.ReadLine());
+Console.Write("Hasta donde ? ");
+int n = int.Parse(Console.ReadLine());
+Tabla(t,n);
---
+++ p71-calificacion-letra/Program.cs
@@ -0,0 +1,23 @@
+﻿// Calificación con letra
+char CalifLetra(float cal) {
+    char letra=' ';
+    if(cal>=90&&cal<=100)
+      letra='A';
+    else if(cal>=80&&cal<90)
+      letra='B';
+    else if(cal>=70&&cal<80)
+    letra='C';
+    else if(cal>=60&&cal<70)
+    letra='D';
+    else if(cal>=0&&cal<60)
+    letra='F';
+    return letra;
+}
+float cal;
+char letra;
+do {
+    Console.Write("Dame tu calificacion (0-100) ? ");
+    cal = float.Parse(Console.ReadLine());
+} while(cal<0 || cal>100);
+letra = CalifLetra(cal);
+Console.WriteLine($"Tu calificación de {cal} en letra es {letra}");
