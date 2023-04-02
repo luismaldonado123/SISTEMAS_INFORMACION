1f1939ea89a319dc14eadbc7faa2de86b0d8723e
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-04-01 22:01:20 -0600

    Add files via upload

---
+++ p75-factorial/Program.cs
@@ -0,0 +1,10 @@
+﻿// Imprime el factorial de un número, usando una función
+double factorial(int n) {
+    double f=1;
+    for(int i=1; i <= n; i++)
+        f=f*i;
+    return f;
+}
+Console.Write("Dame un numero ? ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"\nEl factorial es {factorial(n)}");
---
+++ p76-cuadrado-caracter/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Imprime un cuadro de r x c del caracter especificado, usando una función
+void cuadro(int r, int c, char car) {
+    for(int i=1;i<=r;i++) {
+        for(int j=1;j<=c;j++)
+            Console.Write($"{car} ");
+        Console.WriteLine("");
+    }
+}
+int r,c;
+char car;
+Console.Clear();
+Console.Write("Renglones : "); r = int.Parse(Console.ReadLine());
+Console.Write("Columnas : "); c = int.Parse(Console.ReadLine());
+Console.WriteLine("De que caracter ? ");
+car = Console.ReadLine()[0];
+cuadro(r,c,car);
