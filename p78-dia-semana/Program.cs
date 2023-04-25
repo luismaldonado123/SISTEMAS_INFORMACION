2a43ddcd9c46a661e960e7bbaa81a52b56b3224c
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-04-24 21:43:59 -0600

    Add files via upload

---
+++ p78-dia-semana/Program.cs
@@ -0,0 +1,16 @@
+﻿//programa que obtenga el dia de la semana
+int numero = GetNum();
+string diasemana = GetWeek(numero);
+Console.WriteLine("el dia de la semana es : "+diasemana);
+int GetNum(){
+    int numero;
+    do{
+        Console.WriteLine("Ingresa un numero entre 1 y 7:");
+        numero = int.Parse(Console.ReadLine());
+    }while(numero < 1 || numero > 7);
+    return numero;
+}
+string GetWeek(int numero){
+       string[] DSem = {"Lunes", "Martes","Miercoles","Jueves","Viernes","Sabado","domingo"};
+       return DSem[numero-1];
+}
---
+++ p79-medidas-de-longitud/Program.cs
@@ -0,0 +1,33 @@
+﻿//medidas de longitud con funciones
+Console.Clear();
+int op;
+double pulgadas,centimetros,metros,pies;
+do{
+    Console.WriteLine("Elige una opcion:");
+    Console.WriteLine("Convertir de pulgadas a centimetros.... [1]");
+    Console.WriteLine("Convertir de centimetros a pulgadas.... [2]");
+    op = int.Parse(Console.ReadLine());
+    switch (op){
+        case 1:
+        Console.WriteLine("dame las pulgadas:");
+        pulgadas = double.Parse(Console.ReadLine());
+        centimetros = PA(pulgadas);
+        Console.WriteLine($"las pulgadas son {pulgadas} a centimetros {centimetros}");
+    break;
+    case 2:
+        Console.WriteLine("dame  los metros:");
+        metros = double.Parse(Console.ReadLine());
+        pies = MA(metros);
+        Console.WriteLine($"los metros {metros} a pies son {pies}");
+        break;
+        default:
+        Console.WriteLine("negacion");
+        break;
+    }
+}while(op!=2);
+double MA(double metros){ //metros a pies
+        return metros * 3.28;
+} 
+double PA(double pulgadas){ //pulgadas a centimetros
+    return pulgadas * 2.54;
+}
