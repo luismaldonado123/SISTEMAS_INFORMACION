6ead06bac385daaff60cc653f93229651165c603
luismaldonado123 <124220363+luismaldonado123@users.noreply.github.com>
2023-04-24 21:51:52 -0600

    Add files via upload

---
+++ p80-suma-multiplos/Program.cs
@@ -0,0 +1,28 @@
+﻿//suma multiplos
+int op,inicial,final;
+Console.Clear();
+do{
+    Console.WriteLine("Menu de opciones");
+    Console.WriteLine("suma de los multiplos de 3..... [1]");
+    Console.WriteLine("suma de los multiplos de 4..... [2]");
+    op = int.Parse(Console.ReadLine());
+    switch(op){
+        case 1:
+            Console.WriteLine("rango inicial ?"); inicial = int.Parse(Console.ReadLine());
+            Console.WriteLine("rango final ?"); final = int.Parse(Console.ReadLine());
+            Console.WriteLine($"la suma de los multiplos de 3 entre el rango inicial {inicial} y final {final} es : {sumaM(inicial,final,3)}");
+        break;
+        case 2:
+            Console.WriteLine("rango inicial ?"); inicial = int.Parse(Console.ReadLine());
+            Console.WriteLine("rango final ?"); final = int.Parse(Console.ReadLine());
+            Console.WriteLine($"la suma de los multiplos de 4 entre el rango inicial {inicial} y final {final} es : {sumaM(inicial,final,4)}");
+        break;   
+    }
+}while(op!=2);
+int sumaM(int inicial,int final, int y){
+    int suma = 0;
+    for(int i=inicial; i<=final; i++){
+        suma = suma + i;
+    }
+    return suma;
+}
---
+++ p81-angulo/Program.cs
@@ -0,0 +1,22 @@
+﻿//palabra segun nangulo
+Console.Clear();
+int angulo;
+Console.WriteLine("ingresa un angulo entre 0 y 360 grados");
+angulo = int.Parse(Console.ReadLine());
+string TipoanguloV = Tipoangulo(angulo);
+Console.WriteLine($"el angulo {angulo} es {TipoanguloV}");
+
+string Tipoangulo(int angulo){
+    if (angulo < 90)
+        return "angulo agudo";
+    else if(angulo == 90)
+        return "angulo recto";
+    else if(angulo > 90 && angulo < 180)
+        return "angulo obtuso";
+    else if(angulo == 180)
+        return "angulo llano";
+    else if(angulo > 180 && angulo < 360)
+        return "angulo concavo";
+    else
+        return "opcion invalida";                 
+}
